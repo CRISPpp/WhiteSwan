3d012476092032ebbcddd492d4d8b122992c5577
crisp <2715554797@qq.com>
2022-04-28 17:39:36 +0800

    update category Page

--- src/main/java/com/crisp/saleproject/controller/CategoryController.java
+++ src/main/java/com/crisp/saleproject/controller/CategoryController.java
@@ -6,9 +6,7 @@ import com.crisp.saleproject.common.R;
 import com.crisp.saleproject.entity.Category;
 import com.crisp.saleproject.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类管理
@@ -25,5 +23,17 @@ public class CategoryController {
         wrapper.orderByDesc(Category::getSort);
         categoryService.page(pageInfo, wrapper);
         return R.success(pageInfo);
+    }
+
+    @PostMapping
+    public R<String> addCategory(@RequestBody Category category){
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Category::getName, category.getName());
+        if(categoryService.getOne(wrapper) != null){
+            return R.error("分类已经存在，添加失败");
+        }
+        if(category.getIsDeleted() == null) category.setIsDeleted(0);
+        categoryService.save(category);
+        return R.success("添加成功");
     }
 }
