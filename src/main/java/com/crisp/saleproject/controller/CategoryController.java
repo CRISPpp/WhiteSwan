ec32446d90713867a2d0309775513311343b5771
CRISPpp <2715554797@qq.com>
2022-04-28 12:13:07 +0800

    update service

--- src/main/java/com/crisp/saleproject/controller/CategoryController.java
+++ src/main/java/com/crisp/saleproject/controller/CategoryController.java
@@ -1,7 +1,12 @@
 package com.crisp.saleproject.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.crisp.saleproject.common.R;
+import com.crisp.saleproject.entity.Category;
 import com.crisp.saleproject.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
-    
+    @GetMapping("/page")
+    public R<Page> pageFind(int page, int pageSize){
+        Page pageInfo = new Page(page, pageSize);
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.orderByDesc(Category::getSort);
+        categoryService.page(pageInfo, wrapper);
+        return R.success(pageInfo);
+    }
 }
